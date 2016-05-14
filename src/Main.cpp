2db263bbf801424f91cf4e760aa520b2d938fa76
Alexandre Petit <alexpetit12@hotmail.com>
2016-05-14 17:37:53 -0400

    Add Util.h include

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,22 +5,19 @@
  * Created on May 7, 2016, 3:10 PM
  */
 
-#include <cstdlib>
-#include <iostream>
-
 #include "TickEvent.h"
+#include "Util.h"
 
 std::string type = "TICK";
 std::string instrument = "EUR_USD";
 std::string theTime = "12:34:56";
 double      bid = 10.5;
 double      ask = 10.7;
-    
-int main(int argc, char** argv) {
-    
-    TickEvent T(instrument, theTime, bid, ask);
-    std::cout << T.getType() << std::endl;
-    
+
+int main(int argc, char** argv) 
+{
+    std::string val = util::getEnvironmentVariable("ACCESS_TOKEN");
+    std::cout << val;
     return 0;
 }
 
