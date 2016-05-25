d8088dba6fe93406ebaf89d4810065c0268cfca7
Alexandre Petit <alexpetit12@hotmail.com>
2016-05-24 20:19:23 -0400

    Added basic Streaming example

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,19 +5,25 @@
  * Created on May 7, 2016, 3:10 PM
  */
 
+#include "StreamingForexPrices.h"
 #include "TickEvent.h"
 #include "Util.h"
 
-std::string type = "TICK";
-std::string instrument = "EUR_USD";
-std::string theTime = "12:34:56";
-double      bid = 10.5;
-double      ask = 10.7;
-
 int main(int argc, char** argv) 
 {
-    std::string val = util::getEnvironmentVariable("ACCESS_TOKEN");
-    std::cout << val;
+    std::string accountId = util::getEnvironmentVariable("ACCOUNT_ID");
+    std::string accessToken = util::getEnvironmentVariable("ACCESS_TOKEN");
+    std::string instruments = "USD_CAD";
+    std::string domain = "https://stream-fxpractice.oanda.com";
+
+    eventsQueue eventsQueue;
+    StreamingForexPrices Stream(domain, accessToken, accountId, 
+                                instruments, eventsQueue);
+  
+#if notTests
+    Stream.streamToQueue();
+#endif
+    
     return 0;
 }
 
