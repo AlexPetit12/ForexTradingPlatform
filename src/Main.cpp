83019c40974083ed72569fd4a2f1fef758fb36f1
Alexandre Petit <alexpetit12@hotmail.com>
2016-06-04 16:51:48 -0400

    Added basic threads

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,24 +5,39 @@
  * Created on May 7, 2016, 3:10 PM
  */
 
+#include <thread>
+
 #include "StreamingForexPrices.h"
-#include "TickEvent.h"
+//#include "TickEvent.h"
+#include "Trading.h"
 #include "Util.h"
 
 int main(int argc, char** argv) 
 {
-    std::string accountId = util::getEnvironmentVariable("ACCOUNT_ID");
-    std::string accessToken = util::getEnvironmentVariable("ACCESS_TOKEN");
-    std::string instruments = "USD_CAD";
-    std::string domain = "https://stream-fxpractice.oanda.com";
-
+    std::string accountId =     util::getEnvironmentVariable("ACCOUNT_ID");
+    std::string accessToken =   util::getEnvironmentVariable("ACCESS_TOKEN");
+    std::string instruments =   "USD_CAD";
+    std::string apiDomain =     "https://api-fxpractice.oanda.com";
+    std::string streamDomain =  "https://stream-fxpractice.oanda.com";
+    std::string unitsQty =      "2";
+    
+    // Empty events queue
     eventsQueue eventsQueue;
-    StreamingForexPrices Stream(domain, accessToken, accountId, 
-                                instruments, eventsQueue);
-  
-#if notTests
-    Stream.streamToQueue();
-#endif
+    
+    // Stream
+    StreamingForexPrices stream(streamDomain, accessToken, accountId, instruments, eventsQueue);
+    
+    // Execution
+    Execution execution(apiDomain, accessToken, accountId);
+    
+    // Strategy
+    TestRandomStrategy strategy(instruments, unitsQty, eventsQueue);
+    
+    // Threads
+    std::thread tradeThread(trade, std::ref(eventsQueue), std::ref(strategy), std::ref(execution));
+    std::thread streamThread(&StreamingForexPrices::streamToQueue, stream);
+    tradeThread.join();
+    streamThread.join();
     
     return 0;
 }
