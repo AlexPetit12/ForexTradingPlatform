649c0046676fd99a0cf250db8619a1255e078414
Alexandre Petit <alexpetit12@hotmail.com>
2016-06-04 16:51:10 -0400

    Added trade() function

---
+++ src/Trading.cpp
@@ -0,0 +1,49 @@
+#include "Trading.h"
+
+#include "TickEvent.h"
+
+#include <iostream>
+#include <thread>
+
+void trade(eventsQueue& eventsQueue_, TestRandomStrategy& strategy_, Execution& execution_)
+{
+    while(true)
+    {
+        if(!eventsQueue_.empty())
+        {
+            std::unique_ptr<Event> pUniqueEvent = std::move(eventsQueue_.front());
+            Event* pEvent = pUniqueEvent.get();
+            
+            eventsQueue_.pop();
+            
+            std::string eventType = pEvent->getEventType();
+            
+            if(eventType == "TICK")
+            {
+                std::cout << "TICK\n";
+                TickEvent* pTickEvent = dynamic_cast<TickEvent*>(pEvent);
+                if(pTickEvent == nullptr)
+                {
+                    std::cout << "nullptr in trade() TICK" << std::endl;
+                    return;
+                }
+                strategy_.calculateSignals(pTickEvent);
+            }
+            else if(eventType == "ORDER")
+            {
+                std::cout << "ORDER\n";
+                OrderEvent* pOrderEvent = dynamic_cast<OrderEvent*>(pEvent);
+                if(pOrderEvent == nullptr)
+                {
+                    std::cout << "nullptr in trade() ORDER" << std::endl;
+                    return;
+                }
+                execution_.executeOrder(pOrderEvent);
+            }
+            
+        }
+        
+        // Heartbeat
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+}
---
+++ src/Trading.h
@@ -0,0 +1,10 @@
+#ifndef TRADING_H
+#define TRADING_H
+
+#include "Event.h"
+#include "Execution.h"
+#include "TestRandomStrategy.h"
+
+void trade(eventsQueue& eventsQueue_, TestRandomStrategy& strategy_, Execution& execution_);
+
+#endif /* TRADING_H */
