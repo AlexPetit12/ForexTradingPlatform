2d34f05c470d14ac01baf079e67b5f3dd548f75f
Alexandre Petit <alexpetit12@hotmail.com>
2016-06-19 16:08:02 -0400

    Added some comments

--- src/Position.cpp
+++ src/Position.cpp
@@ -7,10 +7,21 @@
 
 #include "Position.h"
 
+/**
+ * \brief Default constructor
+ */
 Position::Position() 
 {
 }
 
+/**
+ * @param side_
+ * @param market_
+ * @param quantity_
+ * @param exposure_
+ * @param averagePrice_
+ * @param currentPrice_
+ */
 Position::Position(const std::string& side_, const std::string& market_, const std::string& quantity_, 
                    const double& exposure_, const double& averagePrice_, const double& currentPrice_) :
         m_side(side_),
@@ -24,10 +35,16 @@ Position::Position(const std::string& side_, const std::string& market_, const s
 {
 }
 
+/**
+ * \brief Destructor
+ */
 Position::~Position() 
 {
 }
 
+/**
+ * @return Pips
+ */
 double Position::calculatePips()
 {
     double mult = 1.0;
@@ -40,17 +57,26 @@ double Position::calculatePips()
     return mult * (m_currentPrice - m_averagePrice);
 }
 
+/**
+ * @return Profit base
+ */
 double Position::calculateProfitBase()
 {
     double pips = calculatePips();
     return pips * m_exposure / m_currentPrice;
 }
 
+/**
+ * @return Profit percentage
+ */
 double Position::calculateProfitPercentage()
 {
     return m_profitBase / m_exposure * 100;
 }
 
+/**
+ * @param currentPrice_
+ */
 void Position::updatePositionPrice(const double& currentPrice_)
 {
     m_currentPrice     = currentPrice_;
