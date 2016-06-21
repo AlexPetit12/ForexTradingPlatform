11b1de1f400ed7e554bc38327f7cf7c6d4c3d9f3
Alexandre Petit <alexpetit12@hotmail.com>
2016-06-20 20:31:15 -0400

    Added some setters and getters

--- src/SignalEvent.cpp
+++ src/SignalEvent.cpp
@@ -7,10 +7,18 @@
 
 #include "SignalEvent.h"
 
+/**
+ * \brief Default constructor
+ */
 SignalEvent::SignalEvent() 
 {
 }
 
+/**
+ * @param instrument_
+ * @param orderType_
+ * @param side_
+ */
 SignalEvent::SignalEvent(const std::string& instrument_, const std::string& orderType_, 
                          const std::string& side_) :
         Event("SIGNAL", instrument_),
@@ -19,8 +27,40 @@ SignalEvent::SignalEvent(const std::string& instrument_, const std::string& orde
 {
 }
 
-
+/**
+ * brief Destructor
+ */
 SignalEvent::~SignalEvent() 
 {
 }
 
+/**
+ * @param orderType_
+ */
+void SignalEvent::setOrderType(const std::string& orderType_)
+{
+    m_orderType = orderType_;
+}
+
+/**
+ * @param side_
+ */
+void SignalEvent::setSide(const std::string& side_)
+{
+    m_side = side_;
+}
+/**
+ * @return m_orderType
+ */
+std::string SignalEvent::getOrderType() const
+{
+    return m_orderType;
+}
+
+/**
+ * @return m_side
+ */
+std::string SignalEvent::getSide() const
+{
+    return m_side;
+}
--- src/SignalEvent.h
+++ src/SignalEvent.h
@@ -16,6 +16,15 @@ public:
     SignalEvent();
     SignalEvent(const std::string& instrument_, const std::string& orderType_, const std::string& side_);
     virtual ~SignalEvent();
+    
+    // Setters
+    void setOrderType(const std::string& orderType_);
+    void setSide(const std::string& side_);
+    
+    // Getters
+    std::string getOrderType() const;
+    std::string getSide() const;
+    
 private:
     std::string m_orderType;
     std::string m_side;
